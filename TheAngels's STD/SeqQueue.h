5a8fa12f4742321942cb7531158fc3e98efca4b0
theangels <zn373578008@icloud.com>
2015-05-05 18:05:10 +0800

    Updata

---
+++ TheAngels's STD/SeqQueue.h
@@ -0,0 +1,48 @@
+class SeqQueue
+{
+public:
+	SeqQueue(int __len) :len(__len)
+	{
+		que = new ElemType[len];
+		top = 0;
+		tail = 0;
+	}
+public:
+	void push(ElemType add)
+	{
+		if ((tail + 1) % len == top) return;
+		que[tail] = add;
+		tail = (tail + 1) % len;
+		return;
+	}
+public:
+	void pop()
+	{
+		if ((top + 1) % len == tail) return;
+		top = (top + 1) % len;
+	}
+public:
+	void travel()
+	{
+		int p = top;
+		while (p != tail)
+		{
+			cout << que[p];
+			if ((p + 1) % len == tail)
+				cout << endl;
+			else
+				cout << " ";
+			p = (p + 1) % len;
+		}
+	}
+public:
+	~SeqQueue()
+	{
+		delete[]que;
+	}
+private:
+	int len;
+	ElemType *que;
+	int top;
+	int tail;
+};
