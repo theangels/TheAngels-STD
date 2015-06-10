335c8ec972aeeac503b4483bb9582a1f38443729
theangels <zn373578008@icloud.com>
2015-06-10 20:08:27 +0800

    Update

--- TheAngels's STD/Text_Center.cpp
+++ TheAngels's STD/Text_Center.cpp
@@ -3,12 +3,33 @@
 #include "LinkQueue.h"
 #include "LinkList.h"
 #include "Matrix.h"
+#include "AdjMatrix.h"
 int main(void)
 {
-	int t;
-	scanf("%d", &t);
-	for (int _ = 0; _ < t; _++)
+	int n;
+	cin >> n;
+	AdjMatrix<char> tst(n);
+	for (int i = 0; i < n; i++)
 	{
-
+		char point;
+		cin >> point;
+		tst.InsertVex(point);
 	}
+	int m;
+	cin >> m;
+	for (int i = 0; i < m; i++)
+	{
+		char pa, pb;
+		cin >> pa >> pb;
+		tst.InsertArc(pa, pb);
+	}
+	tst.PrintGraph();
+	char delp;
+	cin >> delp;
+	tst.DeleteVex(delp);
+	tst.PrintGraph();
+	char da, db;
+	cin >> da >> db;
+	tst.DeleteArc(da, db);
+	tst.PrintGraph();
 }
