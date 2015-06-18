279f0b3fcbeeefc1aa949e5be04d3ddbce34cb9c
theangels <zn373578008@icloud.com>
2015-06-18 09:09:07 +0800

    Update

--- TheAngels's STD/AdjLink.h
+++ TheAngels's STD/AdjLink.h
@@ -2,24 +2,92 @@ template <typename	VerTexType>
 class AdjLink
 {
 public:
-	AdjLink(int _size = 0) :size(_size){}
+	AdjLink(int _size = 0) :size(_size)
+	{
+		point = 0;
+		for (int i = 0; i < size; i++)
+		{
+			link[i] = new Node;
+			link[i]->id = i;
+			tail[i] = link[i];
+		}
+	}
 public:
 	~AdjLink()
 	{
 
 	}
 public:
-	void InsertArc(VerTexType x, VerTexType y)
+	void Addpoint(VerTexType x)
+	{
+		if (point >= size)return;
+		tab[point] = x;
+		point++;
+	}
+public:
+	void InsertArc(VerTexType x, VerTexType y, int val)
 	{
 		int ax = find(x);
 		int ay = find(y);
-		if (ax == -1 || ay == -1) return;
-
+		if (ax == -1 || ay == -1 || ax >= size || ay >= size) return;
+		insert(ax, ay, val);
+		insert(ay, ax, val);
+	}
+public:
+	void DeleteArc(VerTexType x, VerTexType y)
+	{
+		int ax = find(x);
+		int ay = find(y);
+		if (ax == -1 || ay == -1 || ax >= size || ay >= size) return;
+		remove(ax, ay);
+		remove(ay, ax);
+	}
+public:
+	int VexDegree(VerTexType x)
+	{
+		int ax = find(x);
+		if (ax == -1) return 0;
+		int time = 0;
+		for (Node *p = link[ax]->next; p; p = p->next)
+			time++;
+		return time;
+	}
+public:
+	void PrintGraph()
+	{
+		for (int i = 0; i < size; i++)
+			for (Node *p = link[i]; p; p = p->next)
+			{
+				cout << tab[p->id] << " " << p->val;
+				if (p->next)
+					cout << " -> ";
+				else
+					cout << endl;
+			}
 	}
 private:
-	void insert()
+	void insert(int la, int lb, int val)
 	{
-
+		Node *add = new Node;
+		add->next = tail[la]->next;
+		add->id = lb;
+		add->val = val;
+		tail[la]->next = add;
+		tail[la] = add;
+	}
+private:
+	void remove(int la, int lb)
+	{
+		for (Node *p = link[la]; p->next; p=p->next)
+		{
+			if (p->next->id == lb)
+			{
+				Node *del = p->next;
+				p->next = del->next;
+				delete(del);
+				break;
+			}
+		}
 	}
 private:
 	int find(VerTexType x)
@@ -31,10 +99,14 @@ private:
 private:
 	struct Node
 	{
-		int num;
+		int id;
+		int val;
 		Node *next;
-		Node() :num(), next(){}
+		Node() :id(),val(), next(){}
 	};
 	VerTexType tab[MAX_SIZE];
+	Node *link[MAX_SIZE];
+	Node *tail[MAX_SIZE];
+	int point;
 	int size;
 };
--- TheAngels's STD/Text_Center.cpp
+++ TheAngels's STD/Text_Center.cpp
@@ -7,5 +7,31 @@
 #include "AdjLink.h"
 int main(void)
 {
-	
+	int num;
+	cin >> num;
+	AdjLink<char> tst(num);
+	for (int i = 0; i < num; i++)
+	{
+		char point;
+		cin >> point;
+		tst.Addpoint(point);
+	}
+	int edge;
+	cin >> edge;
+	for (int i = 0; i < edge; i++)
+	{
+		char ta, tb;
+		int val;
+		cin >> ta >> tb >> val;
+		tst.InsertArc(ta, tb, val);
+	}
+	tst.PrintGraph();
+	char da, db;
+	cin >> da >> db;
+	tst.DeleteArc(da, db);
+	tst.PrintGraph();
+	char ask;
+	cin >> ask;
+	cout << tst.VexDegree(ask) << endl;
+
 }
