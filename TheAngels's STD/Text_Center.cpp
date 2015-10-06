4e6d4b99bedc116908ac4ff787a6c29ff00fb165
zn373_000 <zn373578008@icloud.com>
2015-10-06 18:37:26 +0800

    Update

--- TheAngels's STD/Text_Center.cpp
+++ TheAngels's STD/Text_Center.cpp
@@ -1,31 +1,52 @@
-#include "TheAngels's STD.h"
-#include "Seqpoly.h"
-#include "Linkpoly.h"
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int main(void) {
-	
-	Linkpoly tst;
-	tst.Input(1,2);
-	tst.Input(3,4);
-	tst.Input(2,3);
-	tst.Input(-2,3);
-	tst.Input(4,5);
-	tst.Delete(4);
-	tst.Output();
-	double ans = tst.Cal(2);
-	printf("ans = %f\n", ans);
-	/*---------------------------------------------------*/
-	Seqpoly *tst1;
-	init(tst1);
-	insert(tst1,1,2);
-	insert(tst1,4,5);
-	insert(tst1,2,3);
-	insert(tst1,3,4);
-	erase(tst1,4);
-	insert(tst1,-2,3);
-	output(tst1);
-	double ans1 = value(tst1,2);
-	printf("ans1 = %f\n",ans1);
-	
+#include<stdio.h>
+#include<string.h>
+#define MAXLEN 20
+#define MAXROW 5
+#define ElemType int
+typedef struct {
+	char ch[MAXLEN + 1];
+	int length;
+}SString;
+typedef struct Node {
+	int  row, col;
+	ElemType val;
+	struct Node *next;
+} TripleNode;
+typedef struct {
+	int m, n, t;
+	TripleNode *vector[MAXROW + 1];
+} LMatrix;
+int Concat(SString &T, SString S1, SString S2) {
+	if (S1.length + S2.length > MAXLEN)
+		return -1;
+	int t = 1;
+	T.length = S1.length + S2.length;
+	for (int i = 1; i <= S1.length; i++)
+		T.ch[t++] = S1.ch[i];
+	for (int i = 1; i <= S2.length; i++)
+		T.ch[t++] = S2.ch[i];
+	T.ch[t] = '\0';
 	return 0;
+}
+void OutputMartix(LMatrix M) {
+	printf("row col val\n");
+	for (int i = 1; i <= M.m; i++) {
+		TripleNode *tmp = M.vector[i];
+		while (tmp) {
+			printf("%d %d %d\n", tmp->row, tmp->col, tmp->val);
+			tmp = tmp->next;
+		}
+	}
+}
+int main(void) {
+	SString tst, t1, t2;
+	scanf("%s%s", t1.ch + 1, t2.ch + 1);
+	t1.length = strlen(t1.ch + 1);
+	t2.length = strlen(t2.ch + 1);
+	if (Concat(tst, t1, t2) == 0) {
+		printf("%s\n", tst.ch + 1);
+		printf("%d\n", tst.length);
+	}
+	else
+		printf("Overflow!");
 }
