a43fce6bb4c93b8b3e41662fc311abf36ad202f6
郑楠 <zn373578008@outlook.com>
2015-11-21 06:00:55 +0800

    Update

--- TheAngels's STD/Text_Center.cpp
+++ TheAngels's STD/Text_Center.cpp
@@ -2,4 +2,11 @@
 int main(void) {
 	IndexFind tst;
 	tst.OutputIndexList();
+	ElemType add;
+	cin >> add.key >> add.depart;
+	tst.InsertIndexList(add);
+	tst.OutputIndexList();
+	ElemType find;
+	cin >> find.key >> find.depart;
+	cout << tst.SearchIndexList(find) << endl;
 }
--- TheAngels's STD/index.h
+++ TheAngels's STD/index.h
@@ -20,6 +20,7 @@ struct IndexList {
 struct ElemType {
 	KeyType   key;//职工号
 	InfoType depart[15];//部门名称
+	bool aLive;
 	int next;
 };
 struct SSTable {
@@ -30,20 +31,18 @@ struct SSTable {
 class IndexFind {
 public:
 	IndexFind() {
-		cin >> n >> m;
-		A.length = n;
+		cin >> A.length;
 		B.length = 0;
-
 		//CreateMainList
 		for (int i = 1; i <= A.length; i++) {
 			cin >> A.m[i].key >> A.m[i].depart;
 		}
 		//CreateIndexList
-		B.length = 0;
-		for (int i = 1; i <= n; i++) {
+		for (int i = 1; i <= A.length; i++) {
+			A.m[i].aLive = true;
 			A.m[i].next = 0;
 		}
-		for (int i = 1; i <= m; i++) {
+		for (int i = 1; i <= B.length; i++) {
 			tmp[i] = 0;
 		}
 		for (int i = 1; i <= A.length; i++) {
@@ -66,26 +65,60 @@ public:
 			}
 		}
 	}
-
 public:
 	void OutputIndexList() {
 		printf("MainList\n");
 		for (int i = 1; i <= A.length; i++) {
-			printf("%d\t%s\n",A.m[i].key,A.m[i].depart);
+			if (A.m[i].aLive) {
+				printf("%d\t%s\n", A.m[i].key, A.m[i].depart);
+			}
 		}
 		printf("IndexList\n");
 		for (int i = 1; i <= B.length; i++) {
 			int id = B.in[i].start;
 			while (id) {
-				printf("%d\t%s\n", A.m[id].key, A.m[id].depart);
+				if (A.m[i].aLive) {
+					printf("%d\t%s\n", A.m[id].key, A.m[id].depart);
+				}
 				id = A.m[id].next;
 			}
 		}
+	}
+	int SearchIndexList(ElemType worker) {
+		for (int i = 1; i <= A.length; i++) {
+			if (strcmp(B.in[i].index,worker.depart) == 0) {
+				for (int j = B.in[i].start; j != 0; j = A.m[j].next) {
+					if (A.m[j].key == worker.key && A.m[j].aLive) {
+						return j;
+					}
+				}
+			}
+		}
+		return -1;
+	}
+	void InsertIndexList(ElemType worker) {
+		A.length++;
+		A.m[A.length].key = worker.key;
+		strcpy(A.m[A.length].depart, worker.depart);
+		A.m[A.length].next = 0;
+		for (int i = 1; i <= B.length; i++) {
+			if (strcmp(B.in[i].index, worker.depart) == 0) {
+				A.m[tmp[i]].next = A.length;
+				tmp[i] = A.length;
+				return;
+			}
+		}
+		B.length++;
+		strcpy(B.in[B.length].index, worker.depart);
+		B.in[B.length].start = A.length;
+		tmp[B.length] = A.length;
+	}
+	void DeleteIndexList(ElemType worker) {
+		int id = SearchIndexList(worker);
+		A.m[id].aLive = false;
 	}
 private:
 	SSTable A;
 	IndexList B;
 	int tmp[15];
-	int n;//职工数量
-	int m;//部门数量
 };
